da8fd1f42cee2677c9007766ca918269fe0bcc5d
Ravali Jumbarthi <v-rajumb@microsoft.com>
2018-12-01 11:00:49 +0530

    initial code

---
+++ FirstSwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstSwiftApp
+//
+//  Created by Ravali on 01/12/18.
+//  Copyright © 2018 Ravali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
